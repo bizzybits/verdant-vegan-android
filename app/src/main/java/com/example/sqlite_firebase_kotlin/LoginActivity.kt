60303e9705311e233592a1037031d66b46d793bd
bizzybits <ponce.emc@gmail.com>
2022-11-07 10:24:44 -0800

    init commit

---
+++ app/src/main/java/com/example/sqlite_firebase_kotlin/LoginActivity.kt
@@ -0,0 +1,116 @@
+package com.example.sqlite_firebase_kotlin
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.firebaseauthtutorial.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding:ActivityLoginBinding
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please Wait")
+        progressDialog.setMessage("Logging in...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init Firebase
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, open register activity
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        //handle click, begin login
+        binding.loginBtn.setOnClickListener{
+            //before logging in , validate data
+            validateData()
+        }
+
+    }
+
+    private fun validateData() {
+        //get data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailEt.error = "Invalid email format"
+        }
+        else if (TextUtils.isEmpty(password)) {
+            //no password entered
+            binding.passwordEt.error = "Please enter password"
+        }
+        else {
+            //data is validated, begin login
+            firebaseLogin()
+
+        }
+
+    }
+
+    private fun firebaseLogin() {
+        //show progress
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //login success
+                progressDialog.dismiss()
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Logged in as $email", Toast.LENGTH_SHORT).show()
+
+                //open profile
+                startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+
+
+            }
+            .addOnFailureListener{
+                //login failed
+                progressDialog.dismiss()
+                Toast.makeText(this, "Login failed, try creating an account", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        //if user is already logged in go to profile activity
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            //user is already logged in
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sqlite_firebase_kotlin/UserModel.kt
@@ -0,0 +1,5 @@
+package com.example.sqlite_firebase_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+
+class UserModel(val userid: String, val name: String, val age: String)
